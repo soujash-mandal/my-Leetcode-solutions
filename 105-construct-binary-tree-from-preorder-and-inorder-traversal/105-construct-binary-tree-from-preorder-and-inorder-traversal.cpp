f7a11eb4164c57763f398064838ed70172470423
soujash-mandal <70308421+soujash-mandal@users.noreply.github.com>
2022-07-14 13:12:56 +0530

    Time: 42 ms (43.05%), Space: 25.9 MB (92.62%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int idx=0;
+    TreeNode* fun(vector<int>& p,vector<int>& in,int l,int r)
+    {
+        if(idx==p.size()) return NULL;
+        int pos=-1;
+        for(int i=l;i<=r;i++)
+        {
+            if(in[i]==p[idx])
+            {
+                pos=i;
+            }
+        }
+        
+        if(pos==-1) return NULL;
+        
+        TreeNode* root=new TreeNode(p[idx]);
+        idx++;
+        root->left=fun(p,in,l,pos-1);
+        root->right=fun(p,in,pos+1,r);
+        
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& p, vector<int>& in) {
+        int n=p.size();
+        return fun(p,in,0,n-1);
+    }
+};
