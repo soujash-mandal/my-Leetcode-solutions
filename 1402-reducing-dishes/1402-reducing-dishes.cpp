2685b7d1f157888b2976924dee0ded28f2c1c9d4
soujash-mandal <70308421+soujash-mandal@users.noreply.github.com>
2022-07-22 22:43:28 +0530

    Time: 8 ms (61.41%), Space: 8.1 MB (47.06%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -2,35 +2,18 @@ class Solution {
 public:
     int maxSatisfaction(vector<int>& sat) {
         int n=sat.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        vector<set<int>> s(n);
         
-        sort(sat.begin(),sat.end());
-        
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=sat[i];
-        }
+        sort(sat.begin(),sat.end(),greater<int>());
         
         int res=0;
+        int sum=0;
+        int currsum=0;
         
         for(int i=0;i<n;i++)
         {
-            for(int j=1;j<=i;j++)
-            {
-                auto it=s[j-1].end();
-                it--;
-                dp[i][j]=*it+(j+1)*sat[i];
-                
-            }
-            for(int j=0;j<=i;j++)
-            {
-                res=max(res,dp[i][j]);
-                // s[j].insert(max(((j+1)*sat[i]),(dp[i][j])));
-                s[j].insert(dp[i][j]);
-                // cout<<dp[i][j]<<" ";
-            }
-            // cout<<endl;
+            sum+=currsum+sat[i];
+            currsum+=sat[i];
+            res=max(res,sum);
         }
         
         return res;
