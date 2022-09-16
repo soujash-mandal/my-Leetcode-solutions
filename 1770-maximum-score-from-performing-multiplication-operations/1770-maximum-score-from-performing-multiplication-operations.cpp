6520dd14215e70f90e223d9b252069242f2c8982
soujash-mandal <70308421+soujash-mandal@users.noreply.github.com>
2022-09-16 12:39:00 +0530

    Time: 366 ms (95.86%), Space: 122.7 MB (65.15%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,16 +1,5 @@
 class Solution {
-public:
-//     int fun(int l,int r,vector<int>& nums, vector<int>& mul,int idx,int m,vector<vector<int>>& dp)
-//     {
-//         if(idx==m) return 0;
-//         int n=nums.size();
-//         if(dp[l][n-1-r]!=-1) return dp[l][n-1-r];
-//         int res1=mul[idx]*nums[l]+fun(l+1,r,nums,mul,idx+1,m,dp);
-//         int res2=mul[idx]*nums[r]+fun(l,r-1,nums,mul,idx+1,m,dp);
-        
-//         return dp[l][n-1-r]=max(res1,res2);
-//     }
-    
+public: 
     int maximumScore(vector<int>& nums, vector<int>& mul) {
         int n=nums.size();
         int m=mul.size();
@@ -24,14 +13,6 @@ public:
                 dp[i][j]=max(mul[i+j]*nums[i]+dp[i+1][j],mul[i+j]*nums[n-1-j]+dp[i][j+1]);
             }
         }
-        
-        // for(int i=0;i<m+1;i++)
-        // {
-        //     for(int j=0;j<m+1;j++) cout<<dp[i][j]<<" ";
-        //     cout<<endl;
-        // }
         return dp[0][0];
-        
-        // return fun(0,n-1,nums,mul,0,m,dp);
     }
 };
